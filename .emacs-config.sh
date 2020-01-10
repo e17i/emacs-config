b3f1d42d1b38705c560d98b3a1db8a99b90cc9fc
e17i <19799952+e17i@users.noreply.github.com>
2020-01-10 11:16:41 +0100

    added emacs alias config script

---
+++ .emacs-config.sh
@@ -0,0 +1,6 @@
+# this script can be sourced into bash. It provides an alias enabling the
+# emacs config git repo to be used with the actual .emacs.d directory.
+
+STORE=/mnt/c/Users/jko/AppData/Roaming
+
+alias emgit='git --git-dir=$HOME/emacs-config/.git --work-tree=$STORE'
