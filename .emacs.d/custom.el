c57a6e9f49a9c7513f3081dace6f68948f1c0b34
e17i <19799952+e17i@users.noreply.github.com>
2020-01-24 00:20:04 +0100

    splittet init into custom

---
+++ .emacs.d/custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(exec-path
+   '("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/MacPorts/Emacs.app/Contents/MacOS/bin" "/opt/local/bin"))
+ '(fringe-mode 0 nil (fringe))
+ '(gud-mixvm-command-name "/opt/local/bin/mixvm")
+ '(isearch-lazy-count t)
+ '(mac-emulate-three-button-mouse t)
+ '(mouse-drag-copy-region t)
+ '(ns-alternate-modifier 'altername)
+ '(ns-command-modifier 'meta)
+ '(ns-control-modifier 'control)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("marmalade" . "https://marmalade-repo.org/packages/")
+     ("melpa-stable" . "https://stable.melpa.org/packages/")))
+ '(package-selected-packages '(magit ace-jump-zap helm realgud evil org auctex))
+ '(paren-match-face 'paren-face-match-light)
+ '(paren-sexp-mode t)
+ '(scroll-bar-mode nil)
+ '(sentence-end-double-space nil)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "grey50"))))
+ '(fringe ((((class color) (background light)) (:background "grey50")))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,45 +1,14 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;; (package-initialize)
+(package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(exec-path
-   '("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/MacPorts/Emacs.app/Contents/MacOS/bin" "/opt/local/bin"))
- '(fringe-mode 0 nil (fringe))
- '(gud-mixvm-command-name "/opt/local/bin/mixvm")
- '(isearch-lazy-count t)
- '(mac-emulate-three-button-mouse t)
- '(mouse-drag-copy-region t)
- '(ns-alternate-modifier 'altername)
- '(ns-command-modifier 'meta)
- '(ns-control-modifier 'control)
- '(package-archives
-   '(("gnu" . "https://elpa.gnu.org/packages/")
-     ("marmalade" . "https://marmalade-repo.org/packages/")
-     ("melpa-stable" . "https://stable.melpa.org/packages/")))
- '(package-selected-packages '(magit ace-jump-zap helm realgud evil org auctex))
- '(paren-match-face 'paren-face-match-light)
- '(paren-sexp-mode t)
- '(scroll-bar-mode nil)
- '(sentence-end-double-space nil)
- '(size-indication-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "grey50"))))
- '(fringe ((((class color) (background light)) (:background "grey50")))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; (setq org-element-use-cache nil)  ;; temporarily disable org element cache
+;; (org-babel-load-file "~/.emacs.d/init-babel.org")
 
 ;; my customizations
 (load-library "realgud")
