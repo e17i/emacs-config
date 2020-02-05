194a0bec988f8bf43f23144e60520e1cd5b2f1ba
e17i <19799952+e17i@users.noreply.github.com>
2020-02-05 18:51:16 +0100

    added missing keyboard files

---
+++ .emacs.d/keyboard-apple.el
@@ -0,0 +1,6 @@
+(custom-set-variables
+ '(ns-alternate-modifier 'none)
+ '(ns-right-alternate-modifier 'none)
+ '(ns-command-modifier 'meta)
+ '(ns-right-command-modifier 'super)
+ )
---
+++ .emacs.d/keyboard-pc.el
@@ -0,0 +1,7 @@
+(custom-set-variables
+ '(ns-control-modifier 'control)
+ '(ns-alternate-modifier 'meta)
+ '(ns-right-alternate-modifier 'none)
+ '(ns-command-modifier 'super)
+ '(ns-right-command-modifier 'meta)
+ )
