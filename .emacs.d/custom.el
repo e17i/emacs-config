05a2341347b7ba2ef16e5ca2d182dc431307d7ca
e17i <19799952+e17i@users.noreply.github.com>
2020-02-05 18:39:36 +0100

    merge master

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,33 +4,97 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
+ '(buffers-menu-show-directories t)
+ '(c-basic-offset 2)
  '(column-number-mode t)
+ '(comint-completion-autolist t)
  '(custom-file "~/.emacs.d/custom.el")
+ '(delete-selection-mode t)
+ '(dired-recursive-deletes 'top)
+ '(dynamic-completion-mode nil)
+ '(ediff-before-setup-hook
+   '((lambda nil
+       (window-configuration-to-register 101))))
+ '(ediff-quit-hook
+   '(ediff-cleanup-mess
+     (lambda nil
+       (jump-to-register 101))))
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(ediff-suspend-hook
+   '(ediff-default-suspend-function
+     (lambda nil
+       (jump-to-register 101))))
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
+ '(elpy-mode-hook '(hl-line-mode flycheck-mode))
+ '(elpy-modules
+   '(elpy-module-company elpy-module-eldoc elpy-module-folding elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
+ '(evil-default-state 'emacs)
+ '(evil-insert-state-modes nil)
+ '(evil-motion-state-modes nil)
  '(exec-path
    '("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/MacPorts/Emacs.app/Contents/MacOS/bin" "/opt/local/bin"))
  '(fringe-mode 0 nil (fringe))
  '(gud-mixvm-command-name "/opt/local/bin/mixvm")
+ '(hippie-expand-verbose nil)
+ '(history-delete-duplicates t)
+ '(indent-tabs-mode nil)
  '(isearch-lazy-count t)
+ '(magit-git-executable "/opt/local/bin/git")
  '(mouse-drag-copy-region t)
  '(mouse-wheel-scroll-amount '(1 ((meta) . 3) ((shift) . text-scale)))
- '(ns-alternate-modifier 'altername)
+ '(my-frame-transparency 85)
+ '(ns-alternate-modifier 'alternate)
  '(ns-command-modifier 'meta)
  '(ns-control-modifier 'control)
+ '(ns-right-alternate-modifier 'none)
+ '(ns-right-command-modifier 'meta)
+ '(nxml-sexp-element-flag t)
+ '(org-agenda-files nil)
+ '(org-babel-python-command "python2")
+ '(org-goto-auto-isearch nil)
+ '(org-src-preserve-indentation t)
  '(package-archives
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("marmalade" . "https://marmalade-repo.org/packages/")
-     ("melpa-stable" . "https://stable.melpa.org/packages/")))
+     ("melpa-stable" . "https://stable.melpa.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages '(magit ace-jump-zap helm realgud evil org auctex))
+ ;; '(package-selected-packages
+ ;;   '(smartparens tagedit ob-restclient company-restclient restclient py-autopep8 flycheck elpy company-jedi json-mode jedi-direx jedi ox-rst magit zzz-to-char ace-jump-mode helm realgud evil markdown-mode cider web-mode org-plus-contrib js2-mode electric-spacing auctex))
  '(paren-match-face 'paren-face-match-light)
  '(paren-sexp-mode t)
+ '(python-mode-hook
+   '((lambda nil "Turn off Indent Tabs mode."
+       (setq indent-tabs-mode nil))
+     (lambda nil "highlight current column"
+       (highlight-indentation-current-column-mode))
+     (lambda nil "Activate jedi"
+       (jedi:setup))
+     (lambda nil "use superword mode"
+       (superword-mode))))
  '(scroll-bar-mode nil)
+ '(select-enable-clipboard t)
  '(sentence-end-double-space nil)
+ '(server-mode t)
+ '(show-paren-mode t)
  '(size-indication-mode t)
- '(tool-bar-mode nil))
+ '(sp-base-key-bindings nil)
+ '(tool-bar-mode nil)
+ '(wdired-allow-to-change-permissions 'advanced))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "grey50"))))
- '(fringe ((((class color) (background light)) (:background "grey50")))))
+ '(fringe ((((class color) (background light)) (:background "grey50"))))
+ '(ediff-even-diff-A ((t (:background "blue1"))))
+ '(ediff-even-diff-Ancestor ((t (:background "Blue2"))))
+ '(ediff-even-diff-B ((t (:background "Blue3"))))
+ '(ediff-even-diff-C ((t (:background "Blue4"))))
+ '(ediff-odd-diff-A ((t (:background "purple1"))))
+ '(ediff-odd-diff-Ancestor ((t (:background "purple2"))))
+ '(ediff-odd-diff-B ((t (:background "purple3"))))
+ '(ediff-odd-diff-C ((t (:background "purple4"))))
+ '(highlight-indentation-current-column-face ((t (:background "sienna"))))
+ '(highlight-indentation-face ((t (:background "dark slate gray")))))
