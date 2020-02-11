45ac4b7d87a79858cfbe4a2f3183924aca54c5c9
e17i <19799952+e17i@users.noreply.github.com>
2020-02-11 13:52:27 +0100

    keyboard apple now handled in fusion config

--- .emacs.d/keyboard-apple.el
+++ .emacs.d/keyboard-apple.el
@@ -1,5 +1,8 @@
-(setq w32-alt-is-meta nil)
-(setq w32-lwindow-modifier 'Meta)
-(setq w32-pass-lwindow-to-system nil)
-(w32-register-hot-key [M-])
-(w32-register-hot-key [C-g])
+;; all mapping is done in vmware (Profil deutsch optimized for emacs),
+;; so no special handling here..
+(setq w32-alt-is-meta 't)
+(setq w32-lwindow-modifier nil)
+(setq w32-pass-lwindow-to-system 't)
+;; (w32-register-hot-key [M-l])
+;; (w32-register-hot-key [M-])
+;; (w32-register-hot-key [C-g])
