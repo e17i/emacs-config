17d5a8a526d2eaecdae501cdeccdb7ad9255b394
e17i <19799952+e17i@users.noreply.github.com>
2020-03-04 10:34:49 +0100

    keyboard configs are already integrated

--- .emacs.d/keyboard-apple.el
+++
@@ -1,6 +0,0 @@
-(custom-set-variables
- '(ns-alternate-modifier 'none)
- '(ns-right-alternate-modifier 'none)
- '(ns-command-modifier 'meta)
- '(ns-right-command-modifier 'super)
- )
--- .emacs.d/keyboard-pc.el
+++
@@ -1,7 +0,0 @@
-(custom-set-variables
- '(ns-control-modifier 'control)
- '(ns-alternate-modifier 'meta)
- '(ns-right-alternate-modifier 'none)
- '(ns-command-modifier 'super)
- '(ns-right-command-modifier 'meta)
- )
