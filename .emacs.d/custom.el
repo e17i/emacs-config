7520bc27be786ace1d0bd77c0e9955c76c34f03b
e17i <19799952+e17i@users.noreply.github.com>
2021-09-21 12:41:04 +0200

    frame size on x11

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -13,9 +13,6 @@
  '(delete-selection-mode t)
  '(dired-dwim-target t)
  '(dired-recursive-deletes (quote top))
- '(display-buffer-base-action
-   (quote
-    ((display-buffer-maybe-as-scroll-buffer display-buffer-dedicated-window display-buffer--maybe-same-window display-buffer-reuse-window display-buffer--maybe-pop-up-frame-or-window display-buffer-in-previous-window display-buffer-below-selected display-buffer-at-bottom display-buffer-pop-up-frame))))
  '(dynamic-completion-mode nil)
  '(ediff-before-setup-hook
    (quote
@@ -93,7 +90,7 @@
  '(my-environment-mode t)
  '(my-frame-stylings
    (quote
-    (frame-to-top-x11 frame-width-dec frame-height-dec frame-width-dec)))
+    (frame-to-top-x11 frame-width-dec frame-height-dec-two frame-width-dec)))
  '(my-frame-transparency 100)
  '(nxml-sexp-element-flag t)
  '(org-agenda-files nil)
