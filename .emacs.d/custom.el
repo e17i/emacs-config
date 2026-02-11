2f0766f2ac81045be4c0d70210b6b9f66aad81ac
e17i <19799952+e17i@users.noreply.github.com>
2026-02-11 17:26:23 +0100

    delete-selection, eshell prompt extras, compilation regexps

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -10,6 +10,7 @@
  '(column-number-mode t)
  '(comint-completion-autolist t)
  '(custom-file "~/.emacs.d/custom.el")
+ '(deadgrep-project-root-function '(lambda nil default-directory))
  '(default-input-method "german-prefix")
  '(delete-selection-mode t)
  '(dired-dwim-target t)
@@ -50,6 +51,10 @@
  '(evil-default-state 'emacs)
  '(evil-insert-state-modes nil)
  '(evil-motion-state-modes nil)
+ '(evil-undo-system 'undo-tree)
+ '(evil-want-C-h-delete t)
+ '(evil-want-C-u-delete t)
+ '(evil-want-C-u-scroll t)
  '(fill-column 78)
  '(frame-auto-hide-function #'delete-frame)
  '(frame-icon-title-format
@@ -106,6 +111,7 @@
  '(nxml-sexp-element-flag t)
  '(org-agenda-files nil)
  '(org-babel-python-command "python2")
+ '(org-babel-load-languages '((shell . t) (emacs-lisp . t)))
  '(org-goto-auto-isearch nil)
  '(org-src-preserve-indentation t)
  '(package-archives
